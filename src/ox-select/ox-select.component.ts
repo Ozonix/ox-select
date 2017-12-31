41afc1924c4112cbc764c0d62192f8f34595c9dc
Ozonix <xionys@gmail.com>
2017-12-31 13:21:37 +0300

    Fix trigger for header

--- src/ox-select/ox-select.component.ts
+++ src/ox-select/ox-select.component.ts
@@ -7,103 +7,103 @@ import { Component, Input, OnInit, OnChanges, Output, EventEmitter } from '@angu
 })
 export class OxSelectComponent implements OnInit, OnChanges {
 
-  @Input() actionSelect: boolean = false;
-  @Input() multySelect: boolean = false;
-  @Input() filterSelect: boolean = false;
+    @Input() actionSelect: boolean = false;
+    @Input() multySelect: boolean = false;
+    @Input() filterSelect: boolean = false;
 
-  @Input() invalidData: boolean = false;
-  
-  @Input() styleSelect: any = {};
+    @Input() invalidData: boolean = false;
+    
+    @Input() styleSelect: any = {};
 
-  @Input() inputData: any[] = [];
-  @Input() defaultData: string = '- нет данных -';
-  @Input() headerData: string = '';
-  @Input() headerIcon: string = '';
+    @Input() inputData: any[] = [];
+    @Input() defaultData: string = '- нет данных -';
+    @Input() headerData: string = '';
+    @Input() headerIcon: string = '';
 
-  @Output() outputData = new EventEmitter();
-  @Output() outputFilter = new EventEmitter();
+    @Output() outputData = new EventEmitter();
+    @Output() outputFilter = new EventEmitter();
 
-  private searchingData: any[] = [];
-  private selectedData: any[] = [];
-  private selectedShow: any[] = [];
+    private searchingData: any[] = [];
+    private selectedData: any[] = [];
+    private selectedShow: any[] = [];
 
-  private isSelect: any[] = [];
+    private isSelect: any[] = [];
 
-  private init: boolean = false;
-  private listTrigger: boolean = false;
+    private init: boolean = false;
+    private listTrigger: boolean = false;
 
-  private openList(): void {
-      if(this.filterSelect) {
-          this.searchingData = [];
-      }
-      this.listTrigger = !this.listTrigger;
-  }
+    private openList(): void {
+        if(this.filterSelect) {
+            this.searchingData = [];
+        }
+        this.listTrigger = !this.listTrigger;
+    }
 
-  private sendData(data): void {
-      if(!data.disabled) {
+    private sendData(data): void {
+        if(!data.disabled) {
 
-          if(this.multySelect) {
-              const exists = this.isSelect.findIndex((selected: any) => selected.output === data.output);
-              
-              if(exists == -1) {
-                  this.isSelect.push(data);
-                  this.selectedData.push(data.output);
-                  this.selectedShow.push({title:data.input, icon:data.icon || 'fa fa-check-circle'});
-                  const ind = this.inputData.findIndex((inputdata: any) => inputdata.output === data.output);
-                  this.inputData[ind]['selected'] = true;
-              } else {
-                  this.isSelect.splice(exists,1);
-                  this.selectedData.splice(exists,1);
-                  this.selectedShow.splice(exists,1);
-                  const ind = this.inputData.findIndex((inputdata: any) => inputdata.output === data.output);
-                  this.inputData[ind]['selected'] = false;
-              }
+            if(this.multySelect) {
+                const exists = this.isSelect.findIndex((selected: any) => selected.output === data.output);
+                
+                if(exists == -1) {
+                    this.isSelect.push(data);
+                    this.selectedData.push(data.output);
+                    this.selectedShow.push({title:data.input, icon:data.icon || 'fa fa-check-circle'});
+                    const ind = this.inputData.findIndex((inputdata: any) => inputdata.output === data.output);
+                    this.inputData[ind]['selected'] = true;
+                } else {
+                    this.isSelect.splice(exists,1);
+                    this.selectedData.splice(exists,1);
+                    this.selectedShow.splice(exists,1);
+                    const ind = this.inputData.findIndex((inputdata: any) => inputdata.output === data.output);
+                    this.inputData[ind]['selected'] = false;
+                }
 
-          } else {
-              this.selectedData = [];
-              this.selectedShow = [];
-              this.selectedData.push(data.output);
-              this.selectedShow.push({title:data.input, icon:data.icon || 'fa fa-check-circle'});
-              this.listTrigger = false;
-          }
-          
-          this.outputData.emit(this.selectedData);
-      } 
-  }
+            } else {
+                this.selectedData = [];
+                this.selectedShow = [];
+                this.selectedData.push(data.output);
+                this.selectedShow.push({title:data.input, icon:data.icon || 'fa fa-check-circle'});
+                this.listTrigger = false;
+            }
+            
+            this.outputData.emit(this.selectedData);
+        } 
+    }
 
-  private initData(): void {
+    private initData(): void {
 
-      this.selectedData = [];
-      this.selectedShow = [];
-      this.listTrigger = false;
+        this.selectedData = [];
+        this.selectedShow = [];
+        this.listTrigger = false;
 
-      if(this.actionSelect) {
-          this.selectedShow.push({title: this.defaultData});
-      } else {
-          this.inputData.forEach((data)=>{
-              if(data.selected) {
-                  this.sendData(data);
-                  if(data.disabled) {
-                      this.selectedShow.push({title: data.input});
-                  }
-              }
-          });
-      }  
-  }
+        if(this.actionSelect) {
+            this.selectedShow.push({title: this.defaultData});
+        } else {
+            this.inputData.forEach((data)=>{
+                if(data.selected) {
+                    this.sendData(data);
+                    if(data.disabled) {
+                        this.selectedShow.push({title: data.input});
+                    }
+                }
+            });
+        }  
+    }
 
-  private searchData(value): void { 
-      this.searchingData = this.inputData.filter((item) => {
-          return item.input.search(value)!= -1;
-      });
-  }
+    private searchData(value): void { 
+        this.searchingData = this.inputData.filter((item) => {
+            return item.input.search(value)!= -1;
+        });
+    }
 
-  ngOnChanges() {
-      if(this.inputData.length == 0) {
-          this.initData();
-      }
-  }
-  
-  ngOnInit() {
-      this.initData(); 
-  }
+    ngOnChanges() {
+        if(this.inputData.length == 0) {
+            this.initData();
+        }
+    }
+    
+    ngOnInit() {
+        this.initData(); 
+    }
 }
